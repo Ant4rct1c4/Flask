04687687d66a2327d723ccdb2d20da57bed65bbb
NazariiLemyk <nazarii.lemyk.23@pnu.edu.ua>
2026-05-28 01:26:45 +0300

    Copmleted lab2

--- app.py
+++ app.py
@@ -1,10 +1,20 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route('/')
-def home():
-    return "Hello Flask!"
+def resume():
+    return render_template(
+        'resume.html',
+        title='Resume'
+    )
+
+@app.route('/contacts')
+def contacts():
+    return render_template(
+        'contacts.html',
+        title='Contacts'
+    )
 
 if __name__ == '__main__':
     app.run(debug=True)
